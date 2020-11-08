263b927bfa801b6580d73b96a8f17ca076a02089
Jaime Aranzazu <jaedaraz@gmail.com>
2020-11-08 15:33:10 +0100

    add pointcut declaration

--- 024-AOPBefore/src/com/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 024-AOPBefore/src/com/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,15 +2,20 @@ package com.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 //this class is where advices are located
 public class MyDemoLoggingAspect {
+	
+	//use of wildcards (*) to match any add method, (..) independent of num of params
+	@Pointcut("execution( void com.aopdemo.dao.*.add*(..) )")
+	private void pointcutVar() {}
 
 	//Before POINTCUT, specifies where the advice is applied
-	@Before("execution( void com.aopdemo.dao.*.add*(..) )") //use of wildcards (*) to match any add method, (..) independent of num of params
+	@Before("pointcutVar()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("========> Executing @Before advice - Add Method");
 	}
